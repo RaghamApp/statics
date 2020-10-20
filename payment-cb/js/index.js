bd2ca0ab6c3325fad1c0a93e334a2cbca6a61844
Erfan Safari <erfanshield@outlook.com>
2020-10-20 16:44:39 +0330

    Update index.js

--- payment-cb/js/index.js
+++ payment-cb/js/index.js
@@ -15,7 +15,7 @@ var amount = Number(urlParams.get("amount"));
 
 if (userID && amount) {
   console.log("Tracking purchase", userID, transactionID, amount);
-  gtag("config", "G-HYBHD7T8SH", { user_id: userID });
+  gtag("config", "G-HYBHD7T8SH", { user_id: userIDو debug_mode: true });
   gtag("event", "purchase", {
     currency: "USD",
     transaction_id: transactionID,
