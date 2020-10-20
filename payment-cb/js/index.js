09125dc588bd1847b9d6bcd0301b0ed25cc4c8d0
Erfan Safari <erfanshield@outlook.com>
2020-10-20 15:22:23 +0330

    Update index.js

--- payment-cb/js/index.js
+++ payment-cb/js/index.js
@@ -1,5 +1,23 @@
 "use strict";
-
 var urlParams = new URLSearchParams(window.location.search);
-var transactionID = document.getElementById('transactionID');
-transactionID.innerHTML = transactionID.innerHTML.replace('{{transactionID}}', urlParams.get('transactionID') || '');
+var transactionID = document.getElementById("transactionID");
+transactionID.innerHTML = transactionID.innerHTML.replace("{{transactionID}}", urlParams.get("transactionID") || "");
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+const userID = urlParams.get("userID");
+const amount = Number(urlParams.get("amount"));
+
+if (userID && amount) {
+  console.log("Tracking purchase", userID, transactionID, amount);
+  gtag("config", "G-8337L6T0K2", { user_id: userID });
+  gtag("event", "purchase", {
+    currency: "USD",
+    transaction_id: transactionID,
+    value: amount,
+  });
+}
